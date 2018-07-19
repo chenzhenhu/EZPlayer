c50eac6c0b7d20f5c558613bb8395997d0fc8120
chenzhenhu <chenzhenhu5@qq.com>
2018-07-19 14:29:25 +0800

    滑块图标修改

--- EZPlayer/TPPlayerSlider.swift
+++ EZPlayer/TPPlayerSlider.swift
@@ -16,7 +16,7 @@ class TPPlayerSlider: UISlider {
     }
     
     required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: aDecoder)
     }
     
 }

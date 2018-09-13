5f4e8de7bf68df5787c9dd4cb81effc21009a523
chenzhenhu <chenzhenhu5@qq.com>
2018-09-13 13:54:33 +0800

    hh

---
+++ EZPlayer/HHEmbeddedControlView.swift
@@ -0,0 +1,31 @@
+//
+//  HHEmbeddedControlView.swift
+//  EZPlayer
+//
+//  Created by John on 2018/9/13.
+//  Copyright © 2018年 yangjun zhu. All rights reserved.
+//
+
+import UIKit
+
+class HHEmbeddedControlView: UIView {
+
+    @IBOutlet weak var topBarView: UIView!
+    @IBOutlet weak var topBarMaskView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var bottomBarView: UIView!
+    @IBOutlet weak var playTimeLabel: UILabel!
+    @IBOutlet weak var enterFullScreenButton: UIButton!
+    @IBOutlet weak var totalTimeLabel: UILabel!
+    @IBOutlet weak var progressView: UIProgressView!
+    @IBOutlet weak var progressSlider: UISlider!
+    @IBOutlet weak var bottomBarMaskView: UIImageView!
+    
+    @IBOutlet weak var coverImageView: UIImageView!
+    @IBOutlet weak var centerPlayOrPauseButton: UIButton!
+    @IBOutlet weak var seekToLabel: UILabel!
+    
+    
+    
+}
---
+++ EZPlayer/HHFullScreenControlView.swift
@@ -0,0 +1,21 @@
+//
+//  HHFullScreenControlView.swift
+//  EZPlayer
+//
+//  Created by John on 2018/9/13.
+//  Copyright © 2018年 yangjun zhu. All rights reserved.
+//
+
+import UIKit
+
+class HHFullScreenControlView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
